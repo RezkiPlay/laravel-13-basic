72b981e9f157202a00767cd20a561dc93d0a8fa9
RezkiPlay <muhammadrezky006@gmail.com>
2026-07-17 19:23:56 +0800

    Pertemuan 7: Search, Pagination, Filter & Show

--- resources/views/department/show.blade.php
+++ resources/views/department/show.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+
+    <a class="btn btn-warning mb-3" href="{{ route('department.index') }}" role="button">Back</a>
+    {{-- department --}}
+    <h4>Data Department</h4>
+    <ul class="list-group mb-3">
+        <li class="list-group-item">Name : {{ $department->name }}</li>
+        <li class="list-group-item">
+            Created At : {{ $department->created_at->format('d F Y H:i:s') }}</li>
+        <li class="list-group-item">
+            last Update : {{ $department->updated_at->diffForHumans() }}</li>
+
+    </ul>
+    {{-- lecturer --}}
+    <h4>Data Lectures</h4>
+    <ul class="list-group">
+
+        @foreach ($department->lecturers as $lecture)
+            <li class="list-group-item">Name : {{ $lecture->name }}</li>
+        @endforeach
+
+    </ul>
+
+</x-app>
